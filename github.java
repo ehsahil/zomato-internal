2f47323ff59b2d091e08c446c8e63f2e48eec155
Sahil Ahamad <5831392+ehsahil@users.noreply.github.com>
2020-04-12 20:50:12 +0530

    Create github.java

---
+++ github.java
@@ -0,0 +1,16 @@
+	
+static final String JENKINS_URL ="http://stage-jenkins.zomans.com:8080";
+	static final String JENKINS_UserName = "sahil@zomato.com";
+	static final String JENKINS_Password = "d15bf9c7bed04e8b7556e0d98c429d38";
+	static final String JENKINS_SERVICE_PATH ="/zomans/jenkins";
+	static final String JOB_NAME_MASTER ="qa/-magento-deploy/";
+	//static final String JOB_NAME_Preprod = "master";
+	//static final String JOB_NAME_CURRENT = "jabjadsbsd8882";
+	static final String Branch_Group = "production";
+	//String temp = System.getProperty("Build Name");
+    //static final String Job_Group1 = "Te222st222Api2";
+    static final String Branch_Group1 ="Tes2222tA1111pi2";
+	static final String JENKINS_TOKEN_MASTER ="d15bf9c7bed04e8b7556e0d123QA";
+	static final String  JENKINS_TOKEN_PREPROD ="1238836528735226";
+  
+	/*To-hackers-do-not-report-this-is-for-internal-testing*/
